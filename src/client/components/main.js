0d129216c0ea066f21f1eb1e7b64a229a11f376c
Juan Carrera <juankarrera@gmail.com>
2020-02-12 13:48:54 -0500

    add chartjs as a dependency to build charts components

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './home';
+import Charts from './charts'
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home}></Route>
+      <Route exact path="/charts" component={Charts}></Route>
     </Switch>
   )
 }
