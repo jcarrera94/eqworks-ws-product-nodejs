fbf81058b053bf063b631fdd77d58698c343a666
Juan Carrera <juankarrera@gmail.com>
2020-02-13 01:20:08 -0500

    obtain manipulated data to fit events chart

--- src/client/components/chart.js
+++ src/client/components/chart.js
@@ -18,30 +18,58 @@ const ChartId = (props) => {
   }, [chart_id]);
   
   
-  useEffect(() => {
+  useEffect(async () => {
     if (chart_id === 'poi') {
       setState({ ...state, data_type: null});
       //get data for poi
       console.log('first mount', state.chart_id, state.data_type);
     } else {
-      if (state.data_type === 'hourly') {
-        console.log('first mount', state.chart_id, state.data_type);
-        
-      } else {
-        console.log('first mount', state.chart_id, state.data_type);
-        
-      }
+      let chartData = await getChartData(props.api_data, state.chart_id, state.data_type);
+      setData({ ...data, chartData });
+      console.log('!!!!!whats the data', data);
     }
   }, []);
 
+  const getChartData = (rawData, chartId, dataType) => {
+    console.log('@@@@@Did i make it here?????', rawData, chartId, dataType);
+    const obj = {
+      chart_data: {
+        datasets: [{}]
+      }
+    };
 
+    switch (chartId) {
+      case 'events':
+        if (dataType === 'daily') {
+          console.log('Inside switch', obj)
+          obj.chart_type = 'bar';
+          const arr = [];
+          for (let item of rawData[chartId][dataType]) {
+            arr.push({x: item.date.slice(0,10), y: item.events});
+          }
+          obj.chart_data.datasets[0].data = arr;
+          obj.chart_data.datasets[0].label = "Events";
+          obj.chart_data.datasets[0].backgroundColor = "orange";
+          obj.chart_data.datasets[0].hoverBorderWidth = 3,
+          obj.chart_data.datasets[0].hoverBorderColor= "#000";
+        }
+        break;
+      case 'stats':
+        break;
+      case 'poi': 
+        break;
+    }
 
+    console.log('@@@@@obj after', obj)
+
+    return obj;
+  }
   // let data = {
   //   labels: ['1', '2', '3', '4', '5', '6', '7'],
   //   datasets: [
   //     {
   //       label: 'Events',
-  //       data: ['33', '29', '31', '40', '35', '36', '33'],
+  //       data: [22, 29, '31', '40', '35', '36', '33'],
   //       backgroundColor: "green",
   //       hoverBorderWidth: 3,
   //       hoverBorderColor: "#000"
@@ -60,7 +88,7 @@ const ChartId = (props) => {
   return (
     <div>
       <h2>{state.chart_id}</h2>
-      {/* <Chert chartData={data}/> */}
+      {data.chart_type === 'bar' ? <Chert chartData={data.chart_data}/> : <div>Nope</div>}
     </div>
   )
 }
